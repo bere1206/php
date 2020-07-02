bde2b20d40a3b8bb7f467af97a292877b80a35bb
Isela <isela.200@hotmail.com>
2020-07-02 00:03:25 -0500

    commit

---
+++ numerospares.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Números pares</title>
+</head>
+<body>
+	<?php
+		$i=2;
+		
+		for($i; $i<=100; $i= $i+2){ 
+				echo $i."</br>";
+			}
+	?>  
+</body>
+</html>
